0eb380281d89e956053c2ad7ae33f9ffdf60e949
Amit Kumar <offamitkumar@gmail.com>
2020-05-08 14:33:06 +0530

    simple observation

---
+++ Introductory-Problems/Coin-Piles.cpp
@@ -0,0 +1,50 @@
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <cstdio>
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <utility>
+#include <queue>
+#include <stack>
+#include <string>
+#include <cstring>
+#include <cmath>
+#include <map>
+#include <vector>
+#include <array>
+#include <set>
+#include <climits>
+#include <sstream>
+#include <iomanip>
+#include <cassert>
+#include <bitset>
+#include <numeric>
+using namespace std;
+using namespace __gnu_pbds;
+typedef tree< // find_by_order & order_of_key
+        int ,
+        null_type ,
+        less<int> ,
+        rb_tree_tag ,
+        tree_order_statistics_node_update 
+> new_set;
+#define MOD 1000000007
+
+int main(void){
+#ifdef HELL_JUDGE
+    freopen("input","r",stdin);
+    freopen("output","w",stdout);
+    freopen("error","w",stderr);
+#endif 
+    int t; cin>>t;
+    while(t--){
+        int a , b; cin>>a>>b;
+        if(2*a-b>=0 and 2*b-a>=0 && (2*a-b)%3==0 and (2*b-a)%3==0){
+            puts("YES");
+        }else{
+            puts("NO");
+        }
+    }
+    return 0;
+}
