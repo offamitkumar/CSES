5314c179d00998688774e4da8f23c6d44e53ef45
Amit Kumar <offamitkumar@gmail.com>
2020-05-10 15:03:51 +0530

    permuatation of string

---
+++ Introductory-Problems/Creating Strings I.cpp
@@ -0,0 +1,49 @@
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <cstdio>
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <utility>
+#include <queue>
+#include <stack>
+#include <string>
+#include <cstring>
+#include <cmath>
+#include <map>
+#include <vector>
+#include <array>
+#include <set>
+#include <climits>
+#include <sstream>
+#include <iomanip>
+#include <cassert>
+#include <bitset>
+#include <numeric>
+using namespace std;
+using namespace __gnu_pbds;
+typedef tree< // find_by_order & order_of_key
+        int ,
+        null_type ,
+        less<int> ,
+        rb_tree_tag ,
+        tree_order_statistics_node_update 
+> new_set;
+#define MOD 1000000007
+
+int main(void){
+#ifdef HELL_JUDGE
+    freopen("input","r",stdin);
+    freopen("output","w",stdout);
+    freopen("error","w",stderr);
+#endif 
+    string s; cin>>s; sort(s.begin() , s.end());
+    vector<string>v;
+    do{
+        v.push_back(s);
+    }while(next_permutation(s.begin() ,s.end()));
+    cout<<v.size()<<'\n';
+    for(auto&itr:v)
+        cout<<itr<<'\n';
+    return 0;
+}
